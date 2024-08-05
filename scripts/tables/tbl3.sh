63d8fe42b5d16e4a64284a2fd5f2be6422eb2266
Anna Arpaci-Dusseau <annaad@Annas-MacBook-Pro.local>
2024-08-05 07:41:56 -0400

    correct input dir

--- scripts/tables/tbl3.sh
+++ scripts/tables/tbl3.sh
@@ -1,54 +1,54 @@
 # NS-Online
 
-./sansa/bin/ns_final /data/xhchen/livej/graph 10000 5 path 0.1 3 0 0
-./sansa/bin/ns_final /data/xhchen/livej/graph 10000 5 house 0.1 3 0 0
-./sansa/bin/ns_final /data/xhchen/livej/graph 10000 6 dumbbell 0.1 3 0 0
+./sansa/bin/ns_final inputs/livej/graph 10000 5 path 0.1 3 0 0
+./sansa/bin/ns_final inputs/livej/graph 10000 5 house 0.1 3 0 0
+./sansa/bin/ns_final inputs/livej/graph 10000 6 dumbbell 0.1 3 0 0
 
-./sansa/bin/ns_final /data/xhchen/friendster/graph 10000 5 path 0.1 3 0 0
-./sansa/bin/ns_final /data/xhchen/friendster/graph 10000 5 house 0.1 3 0 0
-./sansa/bin/ns_final /data/xhchen/friendster/graph 10000 6 dumbbell 0.1 3 0 0
+./sansa/bin/ns_final inputs/friendster/graph 10000 5 path 0.1 3 0 0
+./sansa/bin/ns_final inputs/friendster/graph 10000 5 house 0.1 3 0 0
+./sansa/bin/ns_final inputs/friendster/graph 10000 6 dumbbell 0.1 3 0 0
 
-./sansa/bin/ns_final /data/xhchen/twitter40/graph 10000 5 path 0.1 3 0 0
-./sansa/bin/ns_final /data/xhchen/twitter40/graph 10000 5 house 0.1 3 0 0
-./sansa/bin/ns_final /data/xhchen/twitter40/graph 10000 6 dumbbell 0.1 3 0 0
+./sansa/bin/ns_final inputs/twitter40/graph 10000 5 path 0.1 3 0 0
+./sansa/bin/ns_final inputs/twitter40/graph 10000 5 house 0.1 3 0 0
+./sansa/bin/ns_final inputs/twitter40/graph 10000 6 dumbbell 0.1 3 0 0
 
 ## Arya - ELP
 
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/livej/graph arya_asap/graph_counting/patterns/5_path /data/xhchen/livej/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/livej/graph arya_asap/graph_counting/patterns/triangle_triangle /data/xhchen/livej/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/livej/graph arya_asap/graph_counting/patterns/5_house /data/xhchen/livej/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/livej/graph arya_asap/graph_counting/patterns/5_path inputs/livej/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/livej/graph arya_asap/graph_counting/patterns/triangle_triangle inputs/livej/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/livej/graph arya_asap/graph_counting/patterns/5_house inputs/livej/graph 1 48 10 0.1
 
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/friendster/graph arya_asap/graph_counting/patterns/5_path /data/xhchen/friendster/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/friendster/graph arya_asap/graph_counting/patterns/triangle_triangle /data/xhchen/friendster/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/friendster/graph arya_asap/graph_counting/patterns/5_house /data/xhchen/friendster/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/friendster/graph arya_asap/graph_counting/patterns/5_path inputs/friendster/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/friendster/graph arya_asap/graph_counting/patterns/triangle_triangle inputs/friendster/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/friendster/graph arya_asap/graph_counting/patterns/5_house inputs/friendster/graph 1 48 10 0.1
 
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/twitter40/graph arya_asap/graph_counting/patterns/5_path /data/xhchen/twitter40/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/twitter40/graph arya_asap/graph_counting/patterns/triangle_triangle /data/xhchen/twitter40/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/twitter40/graph arya_asap/graph_counting/patterns/5_house /data/xhchen/twitter40/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/twitter40/graph arya_asap/graph_counting/patterns/5_path inputs/twitter40/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/twitter40/graph arya_asap/graph_counting/patterns/triangle_triangle inputs/twitter40/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/twitter40/graph arya_asap/graph_counting/patterns/5_house inputs/twitter40/graph 1 48 10 0.1
 
 ## Arya - Run
 
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/livej/graph arya_asap/graph_counting/patterns/5_house 1565584467 48
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/livej/graph arya_asap/graph_counting/patterns/5_path 10138699 48
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/livej/graph arya_asap/graph_counting/patterns/triangle_triangle 1057943054 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/livej/graph arya_asap/graph_counting/patterns/5_house 1565584467 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/livej/graph arya_asap/graph_counting/patterns/5_path 10138699 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/livej/graph arya_asap/graph_counting/patterns/triangle_triangle 1057943054 48
 
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/twitter40/graph arya_asap/graph_counting/patterns/5_house 67728191 48
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/twitter40/graph arya_asap/graph_counting/patterns/5_path 293915 48
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/twitter40/graph arya_asap/graph_counting/patterns/triangle_triangle 217891370 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/twitter40/graph arya_asap/graph_counting/patterns/5_house 67728191 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/twitter40/graph arya_asap/graph_counting/patterns/5_path 293915 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/twitter40/graph arya_asap/graph_counting/patterns/triangle_triangle 217891370 48
 
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/friendster/graph arya_asap/graph_counting/patterns/5_path 143690346 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/friendster/graph arya_asap/graph_counting/patterns/5_path 143690346 48
 
 ## Exact
 
-./sansa/bin/sansa_omp_base /data/xhchen/livej/graph 5path 1 1 0
-./sansa/bin/sansa_omp_base /data/xhchen/livej/graph house 1 1 0
-./sansa/bin/sansa_omp_base /data/xhchen/livej/graph dumbbell 1 1 0
+./sansa/bin/sansa_omp_base inputs/livej/graph 5path 1 1 0
+./sansa/bin/sansa_omp_base inputs/livej/graph house 1 1 0
+./sansa/bin/sansa_omp_base inputs/livej/graph dumbbell 1 1 0
 
-./sansa/bin/sansa_omp_base /data/xhchen/friendster/graph 5path 1 1 0
-./sansa/bin/sansa_omp_base /data/xhchen/friendster/graph house 1 1 0
-./sansa/bin/sansa_omp_base /data/xhchen/friendster/graph dumbbell 1 1 0
+./sansa/bin/sansa_omp_base inputs/friendster/graph 5path 1 1 0
+./sansa/bin/sansa_omp_base inputs/friendster/graph house 1 1 0
+./sansa/bin/sansa_omp_base inputs/friendster/graph dumbbell 1 1 0
 
-./sansa/bin/sansa_omp_base /data/xhchen/twitter40/graph 5path 1 1 0
-./sansa/bin/sansa_omp_base /data/xhchen/twitter40/graph house 1 1 0
-./sansa/bin/sansa_omp_base /data/xhchen/twitter40/graph dumbbell 1 1 0
+./sansa/bin/sansa_omp_base inputs/twitter40/graph 5path 1 1 0
+./sansa/bin/sansa_omp_base inputs/twitter40/graph house 1 1 0
+./sansa/bin/sansa_omp_base inputs/twitter40/graph dumbbell 1 1 0
 
--- scripts/tables/tbl4b.sh
+++ scripts/tables/tbl4b.sh
@@ -3,37 +3,37 @@
 
 # Arya - ELP
 
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/uk2007/graph arya_asap/graph_counting/patterns/triangle /data/xhchen/uk2007/graph 1 48 10 0.1
-#./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/gsh-2015/graph arya_asap/graph_counting/patterns/triangle /data/xhchen/gsh-2015/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/clueweb12/graph arya_asap/graph_counting/patterns/triangle /data/xhchen/clueweb12/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/uk2007/graph arya_asap/graph_counting/patterns/triangle inputs/uk2007/graph 1 48 10 0.1
+#./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/gsh-2015/graph arya_asap/graph_counting/patterns/triangle inputs/gsh-2015/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/clueweb12/graph arya_asap/graph_counting/patterns/triangle inputs/clueweb12/graph 1 48 10 0.1
 
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/uk2007/graph arya_asap/graph_counting/patterns/4_motif/4_clique /data/xhchen/uk2007/graph 1 48 10 0.1
-#./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/gsh-2015/graph arya_asap/graph_counting/patterns/4_motif/4_clique /data/xhchen/gsh-2015/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/clueweb12/graph arya_asap/graph_counting/patterns/4_motif/4_clique /data/xhchen/clueweb12/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/uk2007/graph arya_asap/graph_counting/patterns/4_motif/4_clique inputs/uk2007/graph 1 48 10 0.1
+#./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/gsh-2015/graph arya_asap/graph_counting/patterns/4_motif/4_clique inputs/gsh-2015/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/clueweb12/graph arya_asap/graph_counting/patterns/4_motif/4_clique inputs/clueweb12/graph 1 48 10 0.1
 
 
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/uk2007/graph arya_asap/graph_counting/patterns/5_path /data/xhchen/uk2007/graph 1 48 10 0.1
-#./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/gsh-2015/graph arya_asap/graph_counting/patterns/5_path /data/xhchen/gsh-2015/graph 1 48 10 0.1
-./arya_asap/graph_counting/src/ELP/GraphCounting.out /data/xhchen/clueweb12/graph arya_asap/graph_counting/patterns/5_path /data/xhchen/clueweb12/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/uk2007/graph arya_asap/graph_counting/patterns/5_path inputs/uk2007/graph 1 48 10 0.1
+#./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/gsh-2015/graph arya_asap/graph_counting/patterns/5_path inputs/gsh-2015/graph 1 48 10 0.1
+./arya_asap/graph_counting/src/ELP/GraphCounting.out inputs/clueweb12/graph arya_asap/graph_counting/patterns/5_path inputs/clueweb12/graph 1 48 10 0.1
 
 
 # Arya - count
 
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/uk2007/graph arya_asap/graph_counting/patterns/triangle 19567 48
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/gsh-2015/graph arya_asap/graph_counting/patterns/triangle 5500 48
-./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out /data/xhchen/clueweb12/graph arya_asap/graph_counting/patterns/triangle 16000 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/uk2007/graph arya_asap/graph_counting/patterns/triangle 19567 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/gsh-2015/graph arya_asap/graph_counting/patterns/triangle 5500 48
+./arya_asap/graph_counting/src/multi_thread_single_machine/GraphCounting.out inputs/clueweb12/graph arya_asap/graph_counting/patterns/triangle 16000 48
 
 
 
 # NS-Online 
-./sansa/bin/ns_final /data/xhchen/uk2007/graph 10000 3 triangle 0.1 3 1 0
-./sansa/bin/ns_final /data/xhchen/uk2007/graph 10000 4 4clique 0.1 3 1 0
-./sansa/bin/ns_final /data/xhchen/uk2007/graph 10000 5 path 0.1 3 0 0
+./sansa/bin/ns_final inputs/uk2007/graph 10000 3 triangle 0.1 3 1 0
+./sansa/bin/ns_final inputs/uk2007/graph 10000 4 4clique 0.1 3 1 0
+./sansa/bin/ns_final inputs/uk2007/graph 10000 5 path 0.1 3 0 0
 
-./sansa/bin/ns_final /data/xhchen/clueweb12/graph 10000 3 triangle 0.1 3 1 0
-./sansa/bin/ns_final /data/xhchen/clueweb12/graph 10000 4 4clique 0.1 3 1 0
-./sansa/bin/ns_final /data/xhchen/clueweb12/graph 100000 5 house 0.1 1 0 0
+./sansa/bin/ns_final inputs/clueweb12/graph 10000 3 triangle 0.1 3 1 0
+./sansa/bin/ns_final inputs/clueweb12/graph 10000 4 4clique 0.1 3 1 0
+./sansa/bin/ns_final inputs/clueweb12/graph 100000 5 house 0.1 1 0 0
 
-./sansa/bin/ns_final /data/xhchen/gsh-2015/graph 10000 3 triangle 0.1 3 1 0
-./sansa/bin/ns_final /data/xhchen/gsh-2015/graph 10000 4 4clique 0.1 3 1 0
-./sansa/bin/ns_final /data/xhchen/gsh-2015/graph 100000 5 path 0.1 3 0 0
+./sansa/bin/ns_final inputs/gsh-2015/graph 10000 3 triangle 0.1 3 1 0
+./sansa/bin/ns_final inputs/gsh-2015/graph 10000 4 4clique 0.1 3 1 0
+./sansa/bin/ns_final inputs/gsh-2015/graph 100000 5 path 0.1 3 0 0
